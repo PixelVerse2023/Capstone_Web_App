1a012d5cf97db6d7c39781fce993b13fdadd39e3
PixelVerse2023 <138077455+PixelVerse2023@users.noreply.github.com>
2023-08-09 17:22:38 -0400

    modified port

--- app.py
+++ app.py
@@ -506,4 +506,6 @@ def unauthorized_handler():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to PORT if defined, otherwise use 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
